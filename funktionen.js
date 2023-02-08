2cebd8a5ba87cd2939f246865f48c20029140504
poisonpanicyvy <poisonpanicyvy@web.de>
2023-02-08 09:23:07 +0100

    Klausur eingefügt

--- funktionen.js
+++ funktionen.js
@@ -5,18 +5,61 @@
 
 
 // Wertzuweisung / Assignment
-
+// a = 2; // number
+// a = true; // boolean (true, false)
+// a = "Hallo"; // string //  console.log(typeof a);
+//  console.log(a);
 
 
 
 /* Arrays */
 // Navigation über INDEX
+// let arr;
+// arr = new Array();
+
+
+// arr = [false,true,true,false];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[3]);
+// console.log(arr.length);
+// console.log(arr[arr.length-1]);
+// console.log(arr[5]);
+
+// [09:10] Peter Schmidt
+/* Array -Info, nicht klausurrelevant*/
+// let arr = [];
+// console.log(arr);
+// arr.push("a");
+// console.log(arr);
+// arr.push("b");
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
 
 
 /* Daten-Objekte */
 // Navigation über Bezeichner (keys)
+// let obj = {x:"Hi", y:20};
+// console.log(obj);
+// console.log(obj.x);
+// console.log(obj.y);
 
-/
+let obj = {
+         val0: 10,
+         sub1:{
+             val1: "hi",
+             val2: 2
+             }
+     }; 
+console.log(obj);
+console.log(obj.val0);
+console.log(obj.sub1.val2);
+console.log(obj.sub1.val1);
+console.log(obj.sub1);
+// {{{{}}}} nested object
 
  /*DOM*/
 // DOM  --> Modell der HTMl-Struktur
